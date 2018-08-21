225d4b4195d3a5e5ac7e2fbb12460c3d3f92b93d
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2018-08-21 22:32:33 +0300

    add removing directories possibility

--- FCleaner.c
+++ FCleaner.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 500
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,18 +8,13 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <ftw.h>
 
-int isFile(const char *path) {
-    struct stat pathStat;
-    stat(path, &pathStat);
-    return S_ISREG(pathStat.st_mode);
-}
-
-int isDirectory(const char *path) {
-    struct stat pathStat;
-    stat(path, &pathStat);
-    return S_ISDIR(pathStat.st_mode);
-}
+#ifndef USE_FDS
+#define USE_FDS 15
+#endif
 
 int removeFile(const char *path) {
     FILE *pFile;
@@ -45,11 +42,55 @@ int removeFile(const char *path) {
     fclose(pFile);
     free(buffer);
     if (remove(path) == 0) {
-        printf("The file: '%s' deleted successfully", path);
+        printf("The file: '%s' deleted successfully\n", path);
     } else {
-        printf("Unable to delete: '%s'", path);
+        printf("Unable to delete: '%s'\n", path);
     }
     return 0;
+}
+
+int removeItems(const char *filepath, const struct stat *info,
+                const int typeflag, struct FTW *pathinfo) {
+    if (typeflag == FTW_SL || typeflag == FTW_SLN)
+        remove(filepath);
+    else if (typeflag == FTW_F)
+        removeFile(filepath);
+    else if (typeflag == FTW_DP || typeflag == FTW_D) {
+        rmdir(filepath) ?
+        printf("Cannot delete the directory: '%s'", filepath) :
+        printf("The directory: '%s' deleted successfully\n", filepath);
+    }
+    else if (typeflag == FTW_DNR) {
+        printf(" %s/ (unreadable)\n", filepath);
+        rmdir(filepath);
+    }
+
+    return 0;
+}
+
+
+int removeDirectory(const char *const dirpath) {
+    int result;
+    if (dirpath == NULL || *dirpath == '\0')
+        return errno = EINVAL;
+
+    result = nftw(dirpath, removeItems, USE_FDS, FTW_DEPTH | FTW_PHYS);
+    if (result >= 0)
+        errno = result;
+
+    return errno;
+}
+
+int isFile(const char *path) {
+    struct stat pathStat;
+    stat(path, &pathStat);
+    return S_ISREG(pathStat.st_mode);
+}
+
+int isDirectory(const char *path) {
+    struct stat pathStat;
+    stat(path, &pathStat);
+    return S_ISDIR(pathStat.st_mode);
 }
 
 bool isPathCorrect(const char *path) {
@@ -60,7 +101,11 @@ int clean(const char *path) {
     if (isFile(path)) {
         return removeFile(path);
     } else if (isDirectory(path)) {
-        //todo
+        if (removeDirectory(path)) {
+            fprintf(stderr, "%s.\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
+        return 0;
     }
     return -1;
 }
@@ -73,7 +118,7 @@ int main(int argc, char **argv) {
     if (isPathCorrect(argv[1])) {
         return clean(argv[1]);
     } else {
-        fprintf(stderr, "'%s' not found or not writable", argv[1]);
+        fprintf(stderr, "'%s' not found or not writable\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 }
